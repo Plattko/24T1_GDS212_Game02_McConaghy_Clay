9f4ea19b2e90c43e95d66624816558b509788aad
Clay M - Home <140664778+Plattko@users.noreply.github.com>
2024-02-22 16:13:48 +1100

    Added Points Added Pop-Up UI
    - Added PointsAdded game object as a child of PointsText in the Game scene.
    - Updated PointsUI script to also handle the points added UI. The CountToAddedPoints coroutine differents from the CountToNewPoints coroutine by playing the PointsAdded object's animation.
    - Created Points Added Animation Events script to attached to the points added game object that calls ResetPointsAdded() in the PointsUI script.
    - Created PointsAdded animation that makes the points added text quickly pop up, move upwards and then fade, and reset the current points added and target points added in the PointsUI script.

--- Assets/Scripts/PointsUI.cs
+++ Assets/Scripts/PointsUI.cs
@@ -9,21 +9,35 @@ namespace Plattko
     public class PointsUI : MonoBehaviour
     {
         [SerializeField] private TextMeshProUGUI pointsText;
+        [SerializeField] private TextMeshProUGUI pointsAddedText;
+        [SerializeField] private Animator pointsAddedAnimator;
 
-        private Coroutine CountTo;
+        private Coroutine PointsTextC2;
+        private Coroutine PointsAddedC2;
 
-        private float countDuration = 1f;
+        private float pointsTextCountDuration = 1f;
         private float currentPoints = 0f;
         private float targetPoints = 0f;
+
+        private float pointsAddedCountDuration = 0.2f;
+        private float currentPointsAdded = 0f;
+        private float targetPointsAdded = 0f;
+
+        private void Start()
+        {
+            pointsAddedText.alpha = 0f;
+        }
 
         private void OnEnable()
         {
             PointsManager.OnPointUpdate += UpdatePointsText;
+            PointsManager.OnPointUpdate += UpdatePointsAddedText;
         }
 
         private void OnDisable()
         {
             PointsManager.OnPointUpdate -= UpdatePointsText;
+            PointsManager.OnPointUpdate += UpdatePointsAddedText;
         }
 
         private void UpdatePointsText(int pointsRewarded)
@@ -32,19 +46,19 @@ namespace Plattko
             targetPoints += pointsRewarded;
             
             // Stop the current count to new points coroutine
-            if (CountTo != null)
+            if (PointsTextC2 != null)
             {
-                StopCoroutine(CountTo);
+                StopCoroutine(PointsTextC2);
             }
 
             // Start the count to new points coroutine and assign it to the CountTo coroutine
-            CountTo = StartCoroutine(CountToNewPoints(targetPoints));
+            PointsTextC2 = StartCoroutine(CountToNewPoints(targetPoints));
         }
 
         private IEnumerator CountToNewPoints(float targetPoints)
         {
             // Set the rate to increase the points within the count duration
-            float rate = Mathf.Abs(targetPoints - currentPoints) / countDuration;
+            float rate = Mathf.Abs(targetPoints - currentPoints) / pointsTextCountDuration;
 
             while (currentPoints != targetPoints)
             {
@@ -52,6 +66,52 @@ namespace Plattko
                 pointsText.text = ((int)currentPoints).ToString();
                 yield return null;
             }
+        }
+
+        private void UpdatePointsAddedText(int pointsRewarded)
+        {
+            // Update points added value
+            targetPointsAdded += pointsRewarded;
+
+            // Stop the current count to points added coroutine
+            if (PointsAddedC2 != null)
+            {
+                StopCoroutine(PointsAddedC2);
+            }
+
+            // Start the count to new points coroutine and assign it to the CountTo coroutine
+            PointsAddedC2 = StartCoroutine(CountToAddedPoints(targetPointsAdded));
+        }
+
+        private IEnumerator CountToAddedPoints(float targetPointsAdded)
+        {
+            // Play the points added animation
+            if (!pointsAddedAnimator.enabled)
+            {
+                pointsAddedAnimator.enabled = true;
+            }
+            else
+            {
+                pointsAddedAnimator.Play("PointsAddedNumber", -1, 0);
+                Debug.Log("Else statement");
+            }
+
+            // Set the rate to increase the points within the count duration
+            float rate = Mathf.Abs(targetPointsAdded - currentPointsAdded) / pointsAddedCountDuration;
+
+            while (currentPointsAdded != targetPointsAdded)
+            {
+                currentPointsAdded = Mathf.MoveTowards(currentPointsAdded, targetPointsAdded, rate * Time.deltaTime);
+                pointsAddedText.text = ("+" + (int)currentPointsAdded).ToString();
+                yield return null;
+            }
+        }
+
+        public void ResetPointsAdded()
+        {
+            currentPointsAdded = 0f;
+            targetPointsAdded = 0f;
+            Debug.Log("ResetPointsAdded was called.");
         }
     }
 }
